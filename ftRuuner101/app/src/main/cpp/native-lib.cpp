ffbc502fda8d906feaf937b2b16fd88420a5ed98
luozf12345 <2515187986@qq.com>
2018-03-05 09:52:54 +0800

    创建ftRunner工程，推进fithub

---
+++ ftRuuner101/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,13 @@
+#include <jni.h>
+#include <string>
+
+extern "C"
+JNIEXPORT jstring
+
+JNICALL
+Java_com_example_administrator_ftruuner_1101_LaunchActivity_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
